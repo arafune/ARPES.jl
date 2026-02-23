80004c064cdcd0ff83c6c5899c51ccf425a688a4
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-02-23 15:56:36 +0900

    :heavy_plus_sign: (Add Dates in dependency)

--- src/ARPES.jl
+++ src/ARPES.jl
@@ -1,4 +1,8 @@
 module ARPES
+include("core/arpes_data.jl")
+export kx, ky, kz, phi, psi, eV, Cycle, Ch2, delay
+export ARPESData
+
 include("io/io.jl")
 using .IO
 export load
--- src/core/arpes_data.jl
+++ src/core/arpes_data.jl
@@ -2,17 +2,23 @@ using DimensionalData
 using DimensionalData.Dimensions: @dim
 using Dates
 
-# --- Dimension names used for ARPES ---
-@dim Kx "kx (1/Å)"
+#--- Dimension names used for ARPES ---
+@dim kx "1/Å"
+@dim ky "1/Å"
+@dim kz "1/Å"
 @dim phi "degrees"
-@dim Energy "energy (eV)"
+@dim psi "degrees"
+@dim eV "eV"
 @dim Cycle "cycle"
 @dim Ch2 "ch2"
-@dim Counts "counts"
+@dim delay
 
-struct ARPESData
-    intensity::DimensionalData.AbstractDimArray
-    meta::Dict{String,Any}
+abstract type IntensityUnit end
+struct Counts <: IntensityUnit end
+struct CPS <: IntensityUnit end
+
+struct ARPESData{T<:AbstractDimArray,U<:IntensityUnit}
+    intensity::T
+    unit::U
 end
 
-export ARPESData
