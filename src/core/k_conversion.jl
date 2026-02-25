a9e7ceab8b136f37b1bc2d22a938b355e3a6a7c4
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-02-25 22:34:22 +0900

    :art:

--- src/core/k_conversion.jl
+++ src/core/k_conversion.jl
@@ -1,2 +1,31 @@
 const K_INV = 0.5123167219534328
 
+function mapping(::Type{TypeI}, Ek, α, β_, χ_, δ_, ξ_)
+    k = K_INV * sqrt(Ek)
+    kx =
+        k *
+        ((sin(δ_) * sin(β_) + cos(δ_)*sin(ξ_)*cos(β_))*cos(α) - cos(δ_) * cos(ξ_) * sin(α))
+    ky =
+        k *
+        ((-cos(δ_) * sin(β_) + sin(δ_)*sin(ξ_)*cos(β_))*cos(α) - sin(δ_) * cos(ξ_) * sin(α))
+    return kx, ky
+end
+
+
+function mapping_inverse(::Type{TypeI}, Ek, kx, ky, β0, χ_, δ_, ξ_)
+    k = K_INV * sqrt(Ek)
+    α = arcsin(
+        (sin(ξ_) * sqrt(k^2 - kx^2 - ky^2) - cos(ξ_) * (kx * cos(δ_) + ky * sin(δ))) / k,
+    )
+    β =
+        β0 + arctan(
+            (
+                sin(δ_) * kx - cos(δ_) * ky
+            )/(
+                sin(ξ_) * cos(δ_) * kx +
+                sin(ξ_) * sin(δ_) * ky +
+                cos(ξ_) * sqrt(k^2 - kx^2 - ky-2)
+            ),
+        )
+    return α, β
+end
