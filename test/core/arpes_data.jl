fbc79433d44f5156845fa02c5953a8e6ce1392a2
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-02-26 21:02:04 +0900

    :art: (wrap metadata)

--- test/core/arpes_data.jl
+++ test/core/arpes_data.jl
@@ -11,6 +11,7 @@ arpes_ch_resolved = load(file_path, loc = "SPD")
           (phi(-10.6875:0.5480769230769231:10.6875), eV(4.5:0.005:6.2), detector_ch(1:1:24))
     @test name(arpes_ch_resolved) == :Region1_1
     @test axes(arpes_ch_resolved) == (1:40, 1:341, 1:24)
+    @test metadata(arpes_ch_resolved)[:lens_voltage] == "40V"
     @test arpes_ch_resolved[1, 1, 1] ≈ 67.8969
     @test arpes_ch_resolved.unit == CPS()
 end
