fc92a779995f68505ec0cf915b42f5e489344b86
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-02-27 17:00:22 +0900

    ✅  test for negate, pulse_to_theta

--- test/io/location/location.jl
+++ test/io/location/location.jl
@@ -1,7 +1,7 @@
 using Test
 using ARPES.IO: SPDLoader
 using ARPES: phi, eV, detector_ch, ch2
-using ARPES.IO.Location: canonical_dim
+using ARPES.IO.Location: canonical_dim, pulse_to_theta, negate
 
 @testset "canonical_dim with SPDLoader" begin
     loader = SPDLoader()
@@ -21,3 +21,13 @@ using ARPES.IO.Location: canonical_dim
     @test canonical_dim(loader, :unknown) === nothing
 end
 
+@testset "test for helper function in location.jl" begin
+    @test pulse_to_theta(0) == -45.0
+    @test pulse_to_theta(-6000) == -44.0
+    @test pulse_to_theta(-12000) == -43.0
+    @test pulse_to_theta(-270000) == 0
+    @test pulse_to_theta(-540000) == 45
+    @test negate(5) == -5
+    @test negate(5.3) == -5.3
+    @test negate(-3) == 3
+end
