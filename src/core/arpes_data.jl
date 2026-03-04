ffcf6b5d4ae5cdfd021cf3def94836a21ab270e5
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-03-04 18:52:57 +0900

    Update to use "Base.IndexStyle" rather than just IndexStyle

--- src/core/arpes_data.jl
+++ src/core/arpes_data.jl
@@ -89,7 +89,7 @@ parent(d::ARPESData) = parent(d.intensity)
 
 eltype(::Type{<:ARPESData{A}}) where {A} = eltype(A)
 ndims(d::ARPESData) = ndims(d.intensity)
-IndexStyle(::Type{<:ARPESData{A}}) where {A} = Base.IndexStyle(A)
+Base.IndexStyle(::Type{<:ARPESData{A}}) where {A} = Base.IndexStyle(A)
 Broadcast.broadcastable(d::ARPESData) = Base.Broadcast.broadcastable(d.intensity)
 
 dimtrait(::Type{<:ARPESData}) = DimensionalData.HasDimensionalData()
