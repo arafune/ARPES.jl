b5a6b0f6d242231dffdba76b51314034c7390b82
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-03-08 16:15:24 +0900

    test: 💍 Update test

--- test/dims.jl
+++ test/dims.jl
@@ -12,17 +12,24 @@ end
 
 @testset "Test for shift_dim" begin
     x = range(1, stop = 3, length = 10)
-    y = 1:10
+    y = collect(range(-1, stop = 8, length = 10))
     z = 1:10
     data = rand(10, 10, 10)
     da = DimArray(data, (X(x), Y(y), Z(z)))
     x_dim = da.dims[1]
+    y_dim = da.dims[2]
 
     dim_x_shifted = shift_dim(x_dim, 5)
     @test dim_x_shifted isa DimensionalData.Dimension
     @test dim_x_shifted[1] == 6
     @test dim_x_shifted[end] == 8
     @test length(dim_x_shifted) == length(x_dim)
+
+    dim_y_shifted = shift_dim(y_dim, -2.2)
+    @test dim_y_shifted isa DimensionalData.Dimension
+    @test dim_y_shifted[1] == -3.2
+    @test dim_y_shifted[end] == 5.8
+    @test length(dim_y_shifted) == length(y_dim)
 end
 
 
