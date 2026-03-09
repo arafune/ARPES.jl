c6dcac950a61550820bc74c195512f668637943f
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-03-09 20:41:42 +0900

    test: 💍 Update test

--- test/k_conv/k_conversion.jl
+++ test/k_conv/k_conversion.jl
@@ -24,5 +24,13 @@ end
     @test_logs (:warn, "Missing metadata: χ. Using default value of 0.0 for conversion.") begin
         @test _check_arpesdata(data) == true
     end
+    delete!(metadata(data), :workfunction)
+    @test_throws ArgumentError _check_arpesdata(data)
+
+    data = test_spd_standard()
+    old = dims(data)[dimnum(data, :eV)]
+    new_dim = Dim{:energy}(lookup(old))
+    data = rebuild(data, dims = Base.setindex(dims(data), new_dim, dimnum(data, :eV)))
+    @test_throws ArgumentError _check_arpesdata(data)
     # Further tests can be added here to check the correctness of the conversion.
 end
