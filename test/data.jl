505bdf4ea124766204513888a706241eb13cd491
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-03-17 16:38:48 +0900

    :bookmark: (Bump up to 0.0.10)

--- test/data.jl
+++ test/data.jl
@@ -30,4 +30,5 @@ end
     stack_1_to_3_alt = cat_arpes(data_2_1, data_1_1, data_3_1; dims = :w)
     #cat_arpes(data_2_1, data_1_1, data_3_1; dims = Dim{:w}([1.0, 0.0, 3.0]))
     @test parent(lookup(stack_1_to_3_alt, :w)) == [1.0, 0.0, 3.0]
+    @test_throws ArgumentError cat_arpes(data_2_1, data_1_1, data3; dims = :w)
 end
