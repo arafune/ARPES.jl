52f93b797ca3fce6a6571e0570690e0573cbbda1
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-03-19 22:07:32 +0900

    :bookmark: (Bump up version to 0.0.12)

--- test/dict.jl
+++ test/dict.jl
@@ -9,3 +9,11 @@ using DimensionalData: Dim, hasdim
     d2 = Dict(:a => 1, :b => 3)
     @test merge_consensus(d1, d2) == Dict(:a => 1)
 end
+
+@testset "Test for merge_consensus with conflicted key" begin
+    d1 = Dict(:a => 1, :b => 2)
+    d2 = Dict(:a => 2, :b => 2)  # :a is different from d1
+    d3 = Dict(:a => 3, :b => 2)  # :a appears again
+    @test merge_consensus(d1, d2, d3) == Dict(:b => 2)
+end
+
